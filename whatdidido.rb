02aef2bfe2998115387a106b8f451d682608848a
link-coder100788 <240818174+link-coder100788@users.noreply.github.com>
2026-03-16 09:05:15 -0700

    addded linux support, update to version 1.3.5

--- whatdidido.rb
+++ whatdidido.rb
@@ -4,17 +4,32 @@
 class Whatdidido < Formula
   desc "A pretty wrapper for your shell history"
   homepage "https://link-coder100788.github.io/WhatDidIDo/"
-  url "https://github.com/link-coder100788/WhatDidIDo/archive/refs/tags/v1.3.4.tar.gz"
-  sha256 "4d7d41e2f760c04dbca1bf38eb866e25c5e8f00b135b5679461f9ff16c36f898"
+  url "https://github.com/link-coder100788/WhatDidIDo/archive/refs/tags/v1.3.5.tar.gz"
+  sha256 "6e3e4eca5711f04fea55c59e2b020187f6dc0cc3ed5a8a2b87e14e7f375adb40"
   license "MIT"
   head "https://github.com/link-coder100788/WhatDidIDo.git", branch: "main"
 
-  depends_on :macos
+  depends_on :macos => :build
+  
+  on_macos do
+	  depends_on xcode: ["14.0", :build]
+  end
+  
+  on_linux do
+	  depends_on "swift-lang" => :build
+  end
 
   def install
-    system "xcrun", "swift", "build", "--disable-sandbox", "-c", "release"
-    bin.install ".build/release/whatdidido"
-  end
+	  on_macos do
+		  system "xcrun", "swift", "build", "--disable-sandbox", "-c", "release"
+	  end
+	  
+	  on_linux do
+		  system "swift", "build", "--disable-sandbox", "-c", "release"
+	  end
+	  
+	  bin.install ".build/release/whatdidido"
+	end
 
   test do
     # Verify the binary runs and reports the correct version
