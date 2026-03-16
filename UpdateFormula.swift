c1bb9056fe115a76db47eebd9ed2ea08d7406410
link-coder100788 <240818174+link-coder100788@users.noreply.github.com>
2026-03-16 13:45:17 -0700

    added UpdateFormula

---
+++ UpdateFormula.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+print("Enter migrating version tag")
+let input = readLine()
+
+let process = Process()
+let pipe = Pipe()
+
+process.executableURL = URL(fileURLWithPath: "/bin/zsh")
+process.arguments = [
+	"-c",
+	"curl -L https://github.com/link-coder100788/WhatDidIDo/archive/refs/tags/\(input!).tar.gz | shasum -a 256"
+]
+
+process.standardOutput = pipe
+
+try process.run()
+process.waitUntilExit()
+
+let data = pipe.fileHandleForReading.readDataToEndOfFile()
+let output = String(data: data, encoding: .utf8)!
+
+print(output)
+
